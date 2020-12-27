75d22e67dff2b5eea051b5453f8d5d4d0cfc44a0
Shayan <Shayan.shsh@yahoo.com>
2020-12-27 21:51:02 +0100

    added new section

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -11,12 +11,12 @@
         <SectionB/>
       </div>
 
-      <div id="sectionC" class="vh-100 bg-primary p-4">
+      <div id="sectionC" class="p-4">
         <SectionC/>
       </div>
 
       <div id="sectionD" class="bg-success p-4">
-        test
+        <SectionD/>
       </div>
 
     </div>
@@ -28,10 +28,11 @@ import Navbar from './Navbar';
 import SectionA from './SectionA';
 import SectionB from './SectionB';
 import SectionC from './SectionC';
+import SectionD from './SectionD';
 
 export default {
   name: 'landing-page',
-  components: { SectionC, SectionB, SectionA, Navbar },
+  components: { SectionD, SectionC, SectionB, SectionA, Navbar },
 };
 </script>
 
@@ -46,27 +47,21 @@ export default {
 }
 
 #sectionC {
-  background: #F7F8FA url("../assets/bg-sectionC.png") no-repeat center center;
+  background-color: #F7F8FA;
+}
+
+#sectionD {
+  background: gray url("../assets/bg-sectionC.png") no-repeat center center;
   -webkit-background-size: cover;
   -moz-background-size: cover;
   background-size: cover;
   height: 80vh;
 }
 
-#ride-feed-img {
-  max-height: 80vh;
-}
-
-#knightride-logo-tyoe {
-  width: 70%;
-}
-
-#ios-download {
-  width: 100%;
-}
-
-#android-download {
-  width: 100%;
+@media only screen and (min-width: 600px) {
+  #sectionC {
+    height: 100vh;
+  }
 }
 
 </style>
--- src/components/SectionC.vue
+++ src/components/SectionC.vue
@@ -1,54 +1,42 @@
 <template>
-  <div class="container pt-5">
-
-    <div class="row">
-
-      <div  class="w-75 col-lg-5 pt-sm-5 float-lg-left justify-content-lg-center">
-        <h1  id="header" class="text-white"> {{ header }}</h1>
-      </div>
-      <div  class="col-lg-5 pt-sm-5 float-lg-left justify-content-lg-center">
-        <h1 id="header-small" class="text-white"> {{ header_small }}</h1>
+  <div class="container-fluid">
+    <div class="row justify-content-end ">
+      <div class="col-10 text-left ">
+        <h4 class="display-4 font-weight-bold"> Der Knight</h4>
+        <p class="body">Der Knight besitzt eine Abo-Karte oder eine gekaufte Karte
+          der Verkehrsbetriebe mit dieser er berechtigt ist, je nach Bundesland eine oder mehrere
+          Personen an Wochentagen ab 19Uhr und am Wochenende ganztägig
+          mitzunehmen.
+          Der Knight ist großzügig und warmherzig. Wenn er gebraucht wird fühlt er
+          sich richtig. Unser/e Ritter/in in der goldenen Rüstung eben.</p>
       </div>
     </div>
+    <div class="row justify-content-end pt-4 pt-lg-4">
+      <div class="col-10 text-left ">
+        <h2 class="display-4 font-weight-bold"> Der Rider</h2>
+        <p class="body">Der Rider sucht nach einer Mitfahrgelegenheit und ist kommunikativ.
+          Sein Ziel ist es meistens so günstig und schnell wie möglich von A nach B
+          zu kommen. </p>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+import DownloadIcons from './DownloadIcons';
+
 export default {
   name: 'SectionC',
+  components: { DownloadIcons },
   data() {
-    return {
-      header: 'Finde jetzt deine \n' +
-        'Mitfahrgelegenheit \n' +
-        'oder nehme mit!',
-      header_small: 'Finde jetzt deine \n' +
-        'Mitfahr-' +
-        'gelegenheit \n' +
-        'oder nehme mit!',
-    };
+    return {};
   },
 };
 </script>
 
 <style scoped>
-
-#header{
-  font-size: 7vh;
-  font-weight: bold;
-  text-align: left;
-}
-#header-small{
-  display: none;
-}
-@media only screen and (max-width: 600px) {
-  #header-small{
-    display: block;
-    font-size: 7vh;
-    font-weight: bold;
-    text-align: left;
-  }
-  #header {
-    display: none;
-  }
+.body{
+  font-size: x-large;
 }
 </style>
